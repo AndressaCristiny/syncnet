2e67c76b9ce27150e8da23a1e95f65ab3a3753e3
Andressa Cristiny <andressasilva04178@gmail.com>
2025-09-20 23:50:19 -0300

    fix: removing comments

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,8 @@
-// utils/index.ts
-
 import logger from "./logger";
 import delay from "./delay";
 import handleError from "./handleError";
 import retryRequest from "./retryRequest";
 
-// Agrupamento das funções de transformação em um namespace
 import * as transform from "./transform";
 import * as transformToOZMapData from "./transformToOZMapData";
 
